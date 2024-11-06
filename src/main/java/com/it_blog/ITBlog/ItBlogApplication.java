f1cd1dc86f85e77fc0bdb5354720fb40ea6d75f4
Sergey Kulikov <sergokulik61@gmail.com>
2024-11-06 12:37:54 +0400

    Init project

---
+++ src/main/java/com/it_blog/ITBlog/ItBlogApplication.java
@@ -0,0 +1,13 @@
+package com.it_blog.ITBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it_blog/ITBlog/ItBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it_blog.ITBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
